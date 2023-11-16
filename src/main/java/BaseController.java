37a39f6a7eed6f23f39ca214ffb70c19c24344ff
huming.hm <huming@weidian.com>
2023-11-16 14:20:10 +0800

    add main ----

---
+++ src/main/java/BaseController.java
@@ -0,0 +1,12 @@
+/**
+ * Description:
+ *
+ * @author: huming.hm
+ * @date: 2023-11-16
+ */
+public class BaseController {
+
+    public static void main(String[] args) {
+        System.out.println("AAAA");
+    }
+}

ba18136c97ec8488553a281dc158f8fbaab8aa9a
huming.hm <huming@weidian.com>
2023-11-16 14:27:05 +0800

    mod

--- src/main/java/BaseController.java
+++ src/main/java/BaseController.java
@@ -9,5 +9,6 @@ public class BaseController {
     public static void main(String[] args) {
         System.out.println("AAAA");
         System.out.println("BBB");
+        System.out.println("CCC");
     }
 }

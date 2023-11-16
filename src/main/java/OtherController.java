e0a169aa78e995006baaaba32280e1ed1d095bdf
huming.hm <huming@weidian.com>
2023-11-16 17:34:20 +0800

    mod

--- src/main/java/OtherController.java
+++ src/main/java/OtherController.java
@@ -5,7 +5,8 @@
  * @date: 2023-11-16
  */
 public class OtherController {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        System.out.print("AAA");
     }
 }
